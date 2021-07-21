a440fcb20dc054de8f1674f65b22713d5b5673af
kalg12 <78965771+kalg12@users.noreply.github.com>
2021-07-21 17:00:22 -0500

    modificaciones

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//Tipos de varialbes
+
+var texto = "Hola como estás";
+var x = 20; //númericas
+var flag = true; //bolean
+
+// #maria
+
+var starts=["hola","que onda","hey","Hi"];
+var chat=["¿ah sí?", ["a","b","c"], "apoco"];
+
+function saludar (){
+  console.log(starts[0]);
+
+}
+
+saludar()
+{
+alert(starts[1]);
+}
+
+
+//alert("Hola raza");
+
+//console.log(texto);
